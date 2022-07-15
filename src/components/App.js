c519bf3b8dade0ec35f7c840e308e19725997901
Marcos Ireneo Barbas <barbasmarcosi@gmail.com>
2022-07-15 08:44:29 -0300

    created a functipn to put the body table

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,31 @@
 const App = () => {
+  const putTable = () => [
+    <tr>
+      <td>Rodolfo</td>
+      <td>rodolfo@mail.com</td>
+      <td>rodolfo.com</td>
+    </tr>,
+    <tr>
+      <td>Marcos</td>
+      <td>marcos@mail.com</td>
+      <td>marcos.com</td>
+    </tr>,
+  ];
+
   return (
     <div className="App">
       <table className="Table">
         <thead>
           <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
           </tr>
         </thead>
-        <tbody>
-        <tr>
-            <td>
-              Rodolfo
-            </td>
-            <td>
-              rodolfo@mail.com
-            </td>
-            <td>
-              rodolfo.com
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Marcos
-            </td>
-            <td>
-              marcos@mail.com
-            </td>
-            <td>
-              marcos.com
-            </td>
-          </tr>
-        </tbody>
+        <tbody>{putTable()}</tbody>
       </table>
     </div>
   );
-}
+};
 
 export default App;
